9d25498a63ffa3dd010336266ee23250241ea0fd
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-01-09 12:59:05 +1100

    chore(mechanicjob): remove harvesting and crafting

--- [esx_addons]/esx_mechanicjob/client/main.lua
+++ [esx_addons]/esx_mechanicjob/client/main.lua
@@ -180,77 +180,6 @@ function OpenMechanicActionsMenu()
 	end)
 end
 
-function OpenMechanicHarvestMenu()
-	if Config.EnablePlayerManagement and ESX.PlayerData.job and ESX.PlayerData.job.grade_name ~= 'recrue' then
-		local elements = {
-			{label = _U('gas_can'), value = 'gaz_bottle'},
-			{label = _U('repair_tools'), value = 'fix_tool'},
-			{label = _U('body_work_tools'), value = 'caro_tool'}
-		}
-
-		ESX.UI.Menu.CloseAll()
-
-		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'mechanic_harvest', {
-			title    = _U('harvest'),
-			align    = 'top-left',
-			elements = elements
-		}, function(data, menu)
-			menu.close()
-
-			if data.current.value == 'gaz_bottle' then
-				TriggerServerEvent('esx_mechanicjob:startHarvest')
-			elseif data.current.value == 'fix_tool' then
-				TriggerServerEvent('esx_mechanicjob:startHarvest2')
-			elseif data.current.value == 'caro_tool' then
-				TriggerServerEvent('esx_mechanicjob:startHarvest3')
-			end
-		end, function(data, menu)
-			menu.close()
-			CurrentAction     = 'mechanic_harvest_menu'
-			CurrentActionMsg  = _U('harvest_menu')
-			CurrentActionData = {}
-		end)
-	else
-		ESX.ShowNotification(_U('not_experienced_enough'))
-	end
-end
-
-function OpenMechanicCraftMenu()
-	if Config.EnablePlayerManagement and ESX.PlayerData.job and ESX.PlayerData.job.grade_name ~= 'recrue' then
-		local elements = {
-			{label = _U('blowtorch'),  value = 'blow_pipe'},
-			{label = _U('repair_kit'), value = 'fix_kit'},
-			{label = _U('body_kit'),   value = 'caro_kit'}
-		}
-
-		ESX.UI.Menu.CloseAll()
-
-		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'mechanic_craft', {
-			title    = _U('craft'),
-			align    = 'top-left',
-			elements = elements
-		}, function(data, menu)
-			menu.close()
-
-			if data.current.value == 'blow_pipe' then
-				TriggerServerEvent('esx_mechanicjob:startCraft')
-			elseif data.current.value == 'fix_kit' then
-				TriggerServerEvent('esx_mechanicjob:startCraft2')
-			elseif data.current.value == 'caro_kit' then
-				TriggerServerEvent('esx_mechanicjob:startCraft3')
-			end
-		end, function(data, menu)
-			menu.close()
-
-			CurrentAction     = 'mechanic_craft_menu'
-			CurrentActionMsg  = _U('craft_menu')
-			CurrentActionData = {}
-		end)
-	else
-		ESX.ShowNotification(_U('not_experienced_enough'))
-	end
-end
-
 function OpenMobileMechanicActionsMenu()
 	ESX.UI.Menu.CloseAll()
 
@@ -699,14 +628,6 @@ AddEventHandler('esx_mechanicjob:hasEnteredMarker', function(zone)
 		CurrentAction     = 'mechanic_actions_menu'
 		CurrentActionMsg  = _U('open_actions')
 		CurrentActionData = {}
-	elseif zone == 'Garage' then
-		CurrentAction     = 'mechanic_harvest_menu'
-		CurrentActionMsg  = _U('harvest_menu')
-		CurrentActionData = {}
-	elseif zone == 'Craft' then
-		CurrentAction     = 'mechanic_craft_menu'
-		CurrentActionMsg  = _U('craft_menu')
-		CurrentActionData = {}
 	elseif zone == 'VehicleDeleter' then
 		local playerPed = PlayerPedId()
 
@@ -723,14 +644,6 @@ end)
 AddEventHandler('esx_mechanicjob:hasExitedMarker', function(zone)
 	if zone =='VehicleDelivery' then
 		NPCTargetDeleterZone = false
-	elseif zone == 'Craft' then
-		TriggerServerEvent('esx_mechanicjob:stopCraft')
-		TriggerServerEvent('esx_mechanicjob:stopCraft2')
-		TriggerServerEvent('esx_mechanicjob:stopCraft3')
-	elseif zone == 'Garage' then
-		TriggerServerEvent('esx_mechanicjob:stopHarvest')
-		TriggerServerEvent('esx_mechanicjob:stopHarvest2')
-		TriggerServerEvent('esx_mechanicjob:stopHarvest3')
 	end
 
 	CurrentAction = nil
@@ -923,10 +836,6 @@ Citizen.CreateThread(function()
 
 				if CurrentAction == 'mechanic_actions_menu' then
 					OpenMechanicActionsMenu()
-				elseif CurrentAction == 'mechanic_harvest_menu' then
-					OpenMechanicHarvestMenu()
-				elseif CurrentAction == 'mechanic_craft_menu' then
-					OpenMechanicCraftMenu()
 				elseif CurrentAction == 'delete_vehicle' then
 
 					if Config.EnableSocietyOwnedVehicles then
--- [esx_addons]/esx_mechanicjob/config.lua
+++ [esx_addons]/esx_mechanicjob/config.lua
@@ -29,20 +29,6 @@ Config.Zones = {
 		Type  = 1
 	},
 
-	Garage = {
-		Pos   = { x = -97.5, y = 6496.1, z = 30.4 },
-		Size  = { x = 1.5, y = 1.5, z = 1.0 },
-		Color = { r = 204, g = 204, b = 0 },
-		Type  = 1
-	},
-
-	Craft = {
-		Pos   = { x = -323.140, y = -129.882, z = 37.999 },
-		Size  = { x = 1.5, y = 1.5, z = 1.0 },
-		Color = { r = 204, g = 204, b = 0 },
-		Type  = 1
-	},
-
 	VehicleSpawnPoint = {
 		Pos   = { x = -366.354, y = -110.766, z = 37.696 },
 		Size  = { x = 1.5, y = 1.5, z = 1.0 },
